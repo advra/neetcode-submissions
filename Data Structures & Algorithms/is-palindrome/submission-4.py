a997c0d0bcc7fe37944e0d545f9671459d011e99
Adrian <adriandalonzo@gmail.com>
2026-08-22 14:09:33 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    # two pointer
+    # l,r, check 
+    # find valid char for l (skip)
+    # find valid char for r (skip)
+    # compare
+    # increment with l-> and <=r
+    # skipping problem therefore middle is no half of length
+    #
+    # time: o(n) space: o(1) due to compare in place
+    def isPalindrome(self, s: str) -> bool:
+        l=0
+        r=len(s)-1
+        charL=charR=""
+        while l<r:
+            if s[l].isalnum():
+                charL = s[l].lower()
+            else:
+                l=l+1
+                continue
+            if s[r].isalnum():
+                charR = s[r].lower()
+            else:
+                r=r-1
+                continue
+            if charR != charL:
+                return False
+            else:
+                l=l+1
+                r=r-1
+        return True
+
